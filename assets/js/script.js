97b79643fc23c022660acfaf60a701a9be1ed303
Eulises Cortes <ecortescp@gmail.com>
2024-03-04 13:58:30 -0300

    Se agregan: -Popover en seccion destacados -Link a redes sociales en footer Se presenta la prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
+
+$("ul>li>a").click(function(event){
+    event.preventDefault();
+    let href = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop:$(href).offset().top},1800)
+});
+
+
+
+
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
